8f927e716de16767d6a34bd6fc622da76a8771fc
jonutz93 <manta.ionut@yahoo.com>
2017-03-19 16:49:38 +0200

    Added the three lib

--- client/communication.js
+++ client/communication.js
@@ -1,33 +1,33 @@
-class Communication {
-    constructor(client) {
+var Communication = (function () {
+    function Communication(client) {
     }
-    static CreateComunication(client) {
+    Communication.CreateComunication = function (client) {
         Communication.state = null;
         Communication.client = client;
         console.log(client);
-    }
-    static joinRoom(room) {
+    };
+    Communication.joinRoom = function (room) {
         var gameRoom = Communication.client.join("game");
         gameRoom.onJoin.add(Communication.onJoin);
         gameRoom.onUpdate.add(Communication.update);
         gameRoom.state.listen(Communication.listen);
-    }
-    static SendMessage(message) {
+    };
+    Communication.SendMessage = function (message) {
         console.log('Send', message);
         Communication.client.send(message);
-    }
-    static update(state) {
+    };
+    Communication.update = function (state) {
         console.log(state);
-    }
-    static listen(number, message) {
+    };
+    Communication.listen = function (number, message) {
         console.log('listen', number, message);
-    }
-    static onJoin(client, room) {
+    };
+    Communication.onJoin = function (client, room) {
         console.log(client, "joined", room);
-    }
-}
+    };
+    return Communication;
+}());
 /*
 
    
 */ 
-//# sourceMappingURL=communication.js.map
